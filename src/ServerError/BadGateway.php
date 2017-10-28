8c359318a56d6d9765a564bee76f3d2039c1a299
Nathan Bishop <nbish11@hotmail.com>
2017-10-28 21:47:10 +1000

    Add documentation

--- src/ServerError/BadGateway.php
+++ src/ServerError/BadGateway.php
@@ -4,8 +4,20 @@ namespace Meek\Http\ServerError;
 
 use Meek\Http\ServerError;
 
+/**
+ * ...
+ *
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2016 Nathan Bishop
+ * @license The MIT license.
+ */
 class BadGateway extends ServerError
 {
+    /**
+     * Create a new 'bad gateway' exception.
+     *
+     * @param string[][] $headers Headers to be sent along with the prepared response.
+     */
     public function __construct(array $headers = [])
     {
         parent::__construct(502, 'Bad Gateway', $headers);
