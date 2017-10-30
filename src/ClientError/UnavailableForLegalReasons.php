ffae31059a2d417fd5623a9c61678fe2c82dd7ec
Nathan Bishop <nbish11@hotmail.com>
2017-10-30 10:52:48 +1000

    Add test for 'blocked-by' link

--- src/ClientError/UnavailableForLegalReasons.php
+++ src/ClientError/UnavailableForLegalReasons.php
@@ -4,10 +4,27 @@ namespace Meek\Http\ClientError;
 
 use Meek\Http\ClientError;
 
+/**
+ * Exception class modeling a '451 Unavailable For Legal Reasons' client error.
+ *
+ * @link https://tools.ietf.org/html/rfc7725#section-3
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2016 Nathan Bishop
+ * @license The MIT license.
+ */
 class UnavailableForLegalReasons extends ClientError
 {
-    public function __construct(array $headers = [])
+    /**
+     * Constructs a new 'Unavailable For Legal Reasons' exception.
+     *
+     * @param string $link A URI reference identifying the resource itself.
+     * @param string[][] $headers Headers to be sent along with the prepared response.
+     */
+    public function __construct(string $link, array $headers = [])
     {
+        $link = '<' . $link . '>; rel="blocked-by"';
+        $headers = array_merge($headers, ['link' => [$link]]);
+
         parent::__construct(451, 'Unavailable For Legal Reasons', $headers);
     }
 }
