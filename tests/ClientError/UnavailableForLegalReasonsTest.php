271c288ddf202d150d65a16c2b70e81d49cab959
Nathan Bishop <nbish11@hotmail.com>
2017-10-31 00:47:36 +1000

    Fix broken tests, add missing tests

--- tests/ClientError/UnavailableForLegalReasonsTest.php
+++ tests/ClientError/UnavailableForLegalReasonsTest.php
@@ -12,7 +12,7 @@ class UnavailableForLegalReasonsTest extends TestCase
      */
     public function testIsAClientError()
     {
-        $unavailableForLegalReasons = new UnavailableForLegalReasons();
+        $unavailableForLegalReasons = new UnavailableForLegalReasons('https://example.com/legislation');
 
         $this->assertInstanceOf(ClientError::class, $unavailableForLegalReasons);
     }
@@ -22,7 +22,7 @@ class UnavailableForLegalReasonsTest extends TestCase
      */
     public function testHasCorrectStatusCode()
     {
-        $unavailableForLegalReasons = new UnavailableForLegalReasons();
+        $unavailableForLegalReasons = new UnavailableForLegalReasons('https://example.com/legislation');
 
         $this->assertEquals(451, $unavailableForLegalReasons->getStatusCode());
     }
@@ -32,8 +32,35 @@ class UnavailableForLegalReasonsTest extends TestCase
      */
     public function testHasCorrectReasonPhrase()
     {
-        $unavailableForLegalReasons = new UnavailableForLegalReasons();
+        $unavailableForLegalReasons = new UnavailableForLegalReasons('https://example.com/legislation');
 
         $this->assertEquals('Unavailable For Legal Reasons', $unavailableForLegalReasons->getReasonPhrase());
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\UnavailableForLegalReasons::__construct
+     */
+    public function testAddsBlockedByLinkHeader()
+    {
+        $link = 'https://example.com/legislation';
+        $expected = '<' . $link . '>; rel="blocked-by"';
+        $unavailableForLegalReasons = new UnavailableForLegalReasons($link);
+        $headers = $unavailableForLegalReasons->getHeaders();
+
+        $this->assertArrayHasKey('link', $headers);
+        $this->assertContains($expected, $headers['link']);
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\UnavailableForLegalReasons::__construct
+     */
+    public function testHeadersAreMergedCorrectly()
+    {
+        $link = 'https://example.com/legislation';
+        $unavailableForLegalReasons = new UnavailableForLegalReasons($link, ['connection' => ['close']]);
+        $headers = $unavailableForLegalReasons->getHeaders();
+
+        $this->assertArrayHasKey('link', $headers);
+        $this->assertArrayHasKey('connection', $headers);
     }
 }
