defdf7c04e5f51d54bcfd610f3a7a9322f678f05
Nathan Bishop <nbish11@hotmail.com>
2017-10-31 00:55:57 +1000

    Add documentation and support for letting client know actual content range of resource

--- src/ClientError/RangeNotSatisfiable.php
+++ src/ClientError/RangeNotSatisfiable.php
@@ -4,10 +4,25 @@ namespace Meek\Http\ClientError;
 
 use Meek\Http\ClientError;
 
+/**
+ * Exception modeling the '416 Range Not Satisfiable' HTTP status response.
+ *
+ * @see https://tools.ietf.org/html/rfc7233#section-4.4
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2016 Nathan Bishop
+ * @license The MIT license.
+ */
 class RangeNotSatisfiable extends ClientError
 {
-    public function __construct(array $headers = [])
+    /**
+     * Construct a new 'Range Not Satisfiable' exception.
+     *
+     * @param string $contentRange The valid content range of the target resource.
+     * @param string[][] $headers Additional headers associated with the exception.
+     */
+    public function __construct(string $contentRange, array $headers = [])
     {
+        $headers = array_merge($headers, ['content-range' => [$contentRange]]);
         parent::__construct(416, 'Range Not Satisfiable', $headers);
     }
 }
--- tests/ClientError/RangeNotSatisfiableTest.php
+++ tests/ClientError/RangeNotSatisfiableTest.php
@@ -12,7 +12,7 @@ class RangeNotSatisfiableTest extends TestCase
      */
     public function testIsAClientError()
     {
-        $rangeNotSatisfiable = new RangeNotSatisfiable();
+        $rangeNotSatisfiable = new RangeNotSatisfiable('bytes */47022');
 
         $this->assertInstanceOf(ClientError::class, $rangeNotSatisfiable);
     }
@@ -22,7 +22,7 @@ class RangeNotSatisfiableTest extends TestCase
      */
     public function testHasCorrectStatusCode()
     {
-        $rangeNotSatisfiable = new RangeNotSatisfiable();
+        $rangeNotSatisfiable = new RangeNotSatisfiable('bytes */47022');
 
         $this->assertEquals(416, $rangeNotSatisfiable->getStatusCode());
     }
@@ -32,8 +32,34 @@ class RangeNotSatisfiableTest extends TestCase
      */
     public function testHasCorrectReasonPhrase()
     {
-        $rangeNotSatisfiable = new RangeNotSatisfiable();
+        $rangeNotSatisfiable = new RangeNotSatisfiable('bytes */47022');
 
         $this->assertEquals('Range Not Satisfiable', $rangeNotSatisfiable->getReasonPhrase());
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\RangeNotSatisfiable::__construct
+     */
+    public function testAddsContentRangeHeader()
+    {
+        $contentRange = 'bytes */47022';
+        $rangeNotSatisfiable = new RangeNotSatisfiable($contentRange);
+        $headers = $rangeNotSatisfiable->getHeaders();
+
+        $this->assertArrayHasKey('content-range', $headers);
+        $this->assertContains($contentRange, $headers['content-range']);
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\RangeNotSatisfiable::__construct
+     */
+    public function testHeadersAreMergedCorrectly()
+    {
+        $contentRange = 'bytes */47022';
+        $rangeNotSatisfiable = new RangeNotSatisfiable($contentRange, ['connection' => ['close']]);
+        $headers = $rangeNotSatisfiable->getHeaders();
+
+        $this->assertArrayHasKey('content-range', $headers);
+        $this->assertArrayHasKey('connection', $headers);
     }
 }
