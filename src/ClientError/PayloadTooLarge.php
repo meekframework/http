b77e046d8f9b018402796ee22808762018da3c26
Nathan Bishop <nbish11@hotmail.com>
2017-10-31 00:59:09 +1000

    Add documentation and retry after support as per RFC

--- src/ClientError/PayloadTooLarge.php
+++ src/ClientError/PayloadTooLarge.php
@@ -3,11 +3,31 @@
 namespace Meek\Http\ClientError;
 
 use Meek\Http\ClientError;
+use DateTimeInterface;
 
+/**
+ * Exception modeling the '413 Payload Too Large' HTTP status response.
+ *
+ * @see https://tools.ietf.org/html/rfc7231#section-6.5.11
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2016 Nathan Bishop
+ * @license The MIT license.
+ */
 class PayloadTooLarge extends ClientError
 {
-    public function __construct(array $headers = [])
+    /**
+     * Construct a new 'Payload Too Large' exception.
+     *
+     * @param DateTimeInterface|null $retryAfter How long before the client should retry the request.
+     * @param string[][] $headers Additional headers associated with the exception.
+     */
+    public function __construct(DateTimeInterface $retryAfter = null, array $headers = [])
     {
+        if ($retryAfter) {
+            $retryAfter = [$retryAfter->format('D, d M Y H:i:s \G\M\T')];
+            $headers = array_merge($headers, ['retry-after' => $retryAfter]);
+        }
+
         parent::__construct(413, 'Payload Too Large', $headers);
     }
 }
--- tests/ClientError/PayloadTooLargeTest.php
+++ tests/ClientError/PayloadTooLargeTest.php
@@ -4,6 +4,9 @@ namespace Meek\Http\ClientError;
 
 use PHPUnit\Framework\TestCase;
 use Meek\Http\ClientError;
+use DateTimeZone;
+use DateTime;
+use DateInterval;
 
 class PayloadTooLargeTest extends TestCase
 {
@@ -35,5 +38,59 @@ class PayloadTooLargeTest extends TestCase
         $payloadTooLarge = new PayloadTooLarge();
 
         $this->assertEquals('Payload Too Large', $payloadTooLarge->getReasonPhrase());
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\PayloadTooLarge::__construct
+     */
+    public function testDoesNotAddRetryAfterHeaderIfNotProvided()
+    {
+        $payloadTooLarge = new PayloadTooLarge();
+
+        $this->assertEmpty($payloadTooLarge->getHeaders());
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\PayloadTooLarge::__construct
+     */
+    public function testAddsRetryAfterIfDateTimeProvided()
+    {
+        $timeZone = new DateTimeZone('UTC');
+        $retryAfter = new DateTime('Wed, 25 Oct 2017 13:00:00', $timeZone);
+        $payloadTooLarge = new PayloadTooLarge($retryAfter);
+
+        $headers = $payloadTooLarge->getHeaders();
+
+        $this->assertArrayHasKey('retry-after', $headers);
+        $this->assertContains('Wed, 25 Oct 2017 13:00:00 GMT', $headers['retry-after']);
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\PayloadTooLarge::__construct
+     */
+    public function testRetryAfterHeaderIsFormattedToRFC7231()
+    {
+        $timeZone = new DateTimeZone('UTC');
+        $retryAfter = new DateTime('Wed, 25 Oct 2017 13:00:00', $timeZone);
+        $retryAfter->add(new DateInterval('PT2H')); // 2 hours
+
+        $payloadTooLarge = new PayloadTooLarge($retryAfter);
+        $actual = $payloadTooLarge->getHeaders()['retry-after'][0];
+
+        $this->assertEquals('Wed, 25 Oct 2017 15:00:00 GMT', $actual);
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\PayloadTooLarge::__construct
+     */
+    public function testHeadersAreMergedCorrectly()
+    {
+        $timeZone = new DateTimeZone('UTC');
+        $retryAfter = new DateTime('Wed, 25 Oct 2017 13:00:00', $timeZone);
+        $payloadTooLarge = new PayloadTooLarge($retryAfter, ['connection' => ['close']]);
+        $headers = $payloadTooLarge->getHeaders();
+
+        $this->assertArrayHasKey('retry-after', $headers);
+        $this->assertArrayHasKey('connection', $headers);
     }
 }
