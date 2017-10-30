19348a08c979c1b88406b07d3f83f33b65040e04
Nathan Bishop <nbish11@hotmail.com>
2017-10-31 00:53:47 +1000

    Add documentation and support for retrying request after timeframe

--- src/ClientError/TooManyRequests.php
+++ src/ClientError/TooManyRequests.php
@@ -3,11 +3,31 @@
 namespace Meek\Http\ClientError;
 
 use Meek\Http\ClientError;
+use DateTimeInterface;
 
+/**
+ * Exception modeling the '429 Too Many Requests' HTTP status response.
+ *
+ * @see https://tools.ietf.org/html/rfc6585#section-4
+ * @author Nathan Bishop <nbish11@hotmail.com> (https://nathanbishop.name)
+ * @copyright 2016 Nathan Bishop
+ * @license The MIT license.
+ */
 class TooManyRequests extends ClientError
 {
-    public function __construct(array $headers = [])
+    /**
+     * Construct a new 'Too Many Requests' exception.
+     *
+     * @param DateTimeInterface|null $retryAfter How long before the client should retry the request.
+     * @param string[][] $headers Additional headers associated with the exception.
+     */
+    public function __construct(DateTimeInterface $retryAfter = null, array $headers = [])
     {
+        if ($retryAfter) {
+            $retryAfter = [$retryAfter->format('D, d M Y H:i:s \G\M\T')];
+            $headers = array_merge($headers, ['retry-after' => $retryAfter]);
+        }
+
         parent::__construct(429, 'Too Many Requests', $headers);
     }
 }
--- tests/ClientError/TooManyRequestsTest.php
+++ tests/ClientError/TooManyRequestsTest.php
@@ -4,6 +4,9 @@ namespace Meek\Http\ClientError;
 
 use PHPUnit\Framework\TestCase;
 use Meek\Http\ClientError;
+use DateTimeZone;
+use DateTime;
+use DateInterval;
 
 class TooManyRequestsTest extends TestCase
 {
@@ -35,5 +38,59 @@ class TooManyRequestsTest extends TestCase
         $tooManyRequests = new TooManyRequests();
 
         $this->assertEquals('Too Many Requests', $tooManyRequests->getReasonPhrase());
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\TooManyRequests::__construct
+     */
+    public function testDoesNotAddRetryAfterHeaderIfNotProvided()
+    {
+        $tooManyRequests = new TooManyRequests();
+
+        $this->assertEmpty($tooManyRequests->getHeaders());
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\TooManyRequests::__construct
+     */
+    public function testAddsRetryAfterIfDateTimeProvided()
+    {
+        $timeZone = new DateTimeZone('UTC');
+        $retryAfter = new DateTime('Wed, 25 Oct 2017 13:00:00', $timeZone);
+        $tooManyRequests = new TooManyRequests($retryAfter);
+
+        $headers = $tooManyRequests->getHeaders();
+
+        $this->assertArrayHasKey('retry-after', $headers);
+        $this->assertContains('Wed, 25 Oct 2017 13:00:00 GMT', $headers['retry-after']);
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\TooManyRequests::__construct
+     */
+    public function testRetryAfterHeaderIsFormattedToRFC7231()
+    {
+        $timeZone = new DateTimeZone('UTC');
+        $retryAfter = new DateTime('Wed, 25 Oct 2017 13:00:00', $timeZone);
+        $retryAfter->add(new DateInterval('PT2H')); // 2 hours
+
+        $tooManyRequests = new TooManyRequests($retryAfter);
+        $actual = $tooManyRequests->getHeaders()['retry-after'][0];
+
+        $this->assertEquals('Wed, 25 Oct 2017 15:00:00 GMT', $actual);
+    }
+
+    /**
+     * @covers \Meek\Http\ClientError\TooManyRequests::__construct
+     */
+    public function testHeadersAreMergedCorrectly()
+    {
+        $timeZone = new DateTimeZone('UTC');
+        $retryAfter = new DateTime('Wed, 25 Oct 2017 13:00:00', $timeZone);
+        $tooManyRequests = new TooManyRequests($retryAfter, ['connection' => ['close']]);
+        $headers = $tooManyRequests->getHeaders();
+
+        $this->assertArrayHasKey('retry-after', $headers);
+        $this->assertArrayHasKey('connection', $headers);
     }
 }
